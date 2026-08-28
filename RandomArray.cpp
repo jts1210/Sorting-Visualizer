d1033289715643ed90898d29a31332fd5e6f9ea9
jts1210 <xxjts1210xx@gmail.com>
2026-08-28 02:03:55 -0700

    Refactored main, Algorithm, and RandomArray to use vectors instead of C style pointers. Will make implementing future sorts easier.

--- RandomArray.cpp
+++ RandomArray.cpp
@@ -4,6 +4,7 @@
 
 std::vector<int> RandomArray::generateArray(int numElements) {
 	std::vector<int> arr;
+
 	std::random_device rd;
 	std::mt19937 gen(rd());
 	std::uniform_int_distribution<int> distrib(1, 100);
